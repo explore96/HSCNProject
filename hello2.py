2267c1ed00649393a48eb84f60e4ea46ec6ad034
explore96 <tusag31@gmail.com>
2017-10-04 18:08:47 +0530

    version3.0

---
+++ hello2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Content-type:text/html\r\n\r\n")
+print("<html><body>")
+print("<h1> It works!!! </h1>")
+for i in range(5):
+	print("<h2>Hello World! "+str(i)+"</h2>")
+print("</body></html>")
+
